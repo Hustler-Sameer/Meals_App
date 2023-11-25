bb129d9f0997b77e7f95f605b928476811915e27
Hustler-Sameer <sameerkurkure10@gmail.com>
2023-11-25 12:09:58 +0530

    v4

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
-    Navigator.push(context, route);
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    // .where returns a new list which it gets if the meal category contains category.id which is selected by user
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          meals: filteredMeals,
+          title: category.title,
+        ),
+      ),
+    );
   }
 
   @override
@@ -29,7 +45,13 @@ class CategoriesScreen extends StatelessWidget {
           // *** same as
           // availableCategories.map((category) => CategoryGridItem(category : category)).toList()
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context,
+                    category); // now we will point to this funtion in category_grid_item
+              },
+            )
         ],
       ),
     );
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.meals, required this.title});
@@ -31,11 +32,7 @@ class MealsScreen extends StatelessWidget {
       content = ListView.builder(
         itemCount: meals.length,
         itemBuilder: (ctx, index) {
-          print(meals[index].title);
-          return Text(
-            // remember to return
-            meals[index].title,
-          );
+          return MealItem(meal: meals[index]);
         },
       );
     }
