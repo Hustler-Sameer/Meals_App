bb36304182e9160b4e9eb60439e9ed09783a6a19
Hustler-Sameer <sameerkurkure10@gmail.com>
2023-11-26 20:36:43 +0530

    v4

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  //working with enum to display them as all Caps
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,11 +33,14 @@ class MealItem extends StatelessWidget {
             FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
             ),
             Positioned(
               bottom: 0,
               left: 0,
-              right: 50,
+              right: 0,
               child: Container(
                 color: Colors.black54,
                 padding:
@@ -48,7 +63,23 @@ class MealItem extends StatelessWidget {
                       height: 12,
                     ),
                     Row(
-                      children: [],
+                      // displaying all meta data
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: ('${meal.duration} min'),
+                        ), // this can also be written meal.duration.toString()
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                            icon: Icons.attach_money, label: affordabilityText)
+                      ],
                     )
                   ],
                 ),
